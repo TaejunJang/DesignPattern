543acd9b05b7d9b44a8c1dea279f508e49c290e7
jtj <jtj1019@meatbox.co.kr>
2022-03-15 00:24:19 +0900

    #레이지 홀더 방식 싱글톤 구현

---
+++ src/pattern/singleton/SingletonType4.java
@@ -0,0 +1,22 @@
+package pattern.singleton;
+
+public class SingletonType4 {
+
+	
+	private static SingletonType4 instance;
+	
+	//외부에서 객체 생성 불가
+	private SingletonType4() {
+		
+	}
+	
+	//클래스 내부에 정적 클래스를 추가
+	private static class InnerInstanceClazz	{
+		private static final SingletonType4 singletonInstance = new SingletonType4();
+	}
+	
+	public static SingletonType4 getInstance() {
+		return InnerInstanceClazz.singletonInstance;
+	}
+	
+}
